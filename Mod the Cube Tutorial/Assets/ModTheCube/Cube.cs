f6951a648102511f9834cfc85d722ba04537b9fe
Yazeed Williams <yazeedwilliams13@gmail.com>
2025-01-20 10:42:08 +0200

    material changes after a few seconds

--- Mod the Cube Tutorial/Assets/ModTheCube/Cube.cs
+++ Mod the Cube Tutorial/Assets/ModTheCube/Cube.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+
+    private float time = 0f;
+
+    private Material material;
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
-        Material material = Renderer.material;
-        
+        transform.position = new Vector3(0, 0, 0);
+        transform.localScale = Vector3.one * 1.5f;
+
+        material = Renderer.material;
+
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
     }
-    
+
     void Update()
     {
+        time += Time.deltaTime;
         transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+
+        if (time >= 3)
+        {
+            material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 0.04f);
+            time = 0f;
+        }
     }
 }
