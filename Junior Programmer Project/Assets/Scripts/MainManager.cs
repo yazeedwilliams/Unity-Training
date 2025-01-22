2924436d6ae60042cb61a59cf7b38f3f49a01237
Yazeed Williams <yazeedwilliams13@gmail.com>
2025-01-22 12:26:31 +0200

    Data persistence between scenes

---
+++ Junior Programmer Project/Assets/Scripts/MainManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
